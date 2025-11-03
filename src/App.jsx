c94de14f156a70b436f3d30b4403ff928b224cbb
Estio WSL Environment <estio0011@gmail.com>
2025-11-04 00:04:04 +0200

    feat: handle touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,61 @@
-import {useEffect, useRef, useState} from "react"
+import { useEffect, useRef, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import {clamp} from "./utils/math"
+import { clamp } from "./utils/math";
 
 function App() {
-    const [yPos, setYPos] = useState(0)
-    const homeRef = useRef()
-    const aboutRef = useRef()
-    const handleScroll = (e) => {
-        setYPos((oldValue) => {
-            const newValue = oldValue + e.deltaY
-            // Handle scrolling
-            if (newValue >= 500) aboutRef.current.scrollIntoView({behavior: "smooth"})
-            else homeRef.current.scrollIntoView({behavior: "smooth"})
-            // Save scroll position
-            return clamp(newValue, 0, 1000)
-        })
-    }
+  const initTouchYRef = useRef(-1);
+  const [yPos, setYPos] = useState(0);
+  const homeRef = useRef();
+  const aboutRef = useRef();
 
-    useEffect(() => {
-        window.addEventListener("wheel", handleScroll)
-        window.addEventListener("touchmove", handleScroll)
+  const handleYPositionChange = (value) => {
+    setYPos((oldValue) => {
+      const newValue = oldValue + value;
+      // Handle scrolling
+      if (newValue >= 500)
+        aboutRef.current.scrollIntoView({ behavior: "smooth" });
+      else homeRef.current.scrollIntoView({ behavior: "smooth" });
+      // Save scroll position
+      return clamp(newValue, 0, 1000);
+    });
+  };
 
-        return () => {
-            window.removeEventListener("wheel", handleScroll)
-            window.removeEventListener("touchmove", handleScroll)
-        }
-    }, [])
-    return (
-        <div className="content">
-            <div
-                ref={homeRef}
-                className="content-section">
-                <h1>{yPos}</h1>
-            </div>
-            <div
-                ref={aboutRef}
-                className="content-section">
-                <h1>{yPos}</h1>
-            </div>
-        </div>
-    )
+  const handleScroll = (e) => handleYPositionChange(e.deltaY);
+  const handleTouch = (e) => {
+    if (initTouchYRef.current === -1) return; // Skip if we don't have initial touch
+    const deltaY = initTouchYRef.current - e.touches[0].clientY;
+    console.log(deltaY);
+
+    handleYPositionChange(deltaY);
+  };
+  const handleTouchDown = (e) => (initTouchYRef.current = e.touches[0].clientY);
+  const handleTouchEnd = () => (initTouchYRef.current = -1);
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleScroll);
+    window.addEventListener("touchstart", handleTouchDown);
+    window.addEventListener("touchmove", handleTouch);
+    window.addEventListener("touchend", handleTouchEnd);
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+      window.removeEventListener("touchstart", handleTouchDown);
+      window.removeEventListener("touchmove", handleTouch);
+      window.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, []);
+  return (
+    <div className="content">
+      <div ref={homeRef} className="content-section">
+        <h1>{yPos}</h1>
+      </div>
+      <div ref={aboutRef} className="content-section">
+        <h1>{yPos}</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
